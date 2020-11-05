11f2c6a5378f93c3dd76f90108b6239587d242b5
rushjs1 <60147451+rushjs1@users.noreply.github.com>
2020-11-04 21:34:38 -0500

    push

--- homework5/public/app/app.js
+++ homework5/public/app/app.js
@@ -1,6 +1,13 @@
 import * as Model from "../model/model.js";
 
+function initListeners() {
+  $("#genres").change(function() {
+    Model.getStudentByClassNumber(this.value);
+    console.log(this.value);
+  });
+}
+
 $(document).ready(function() {
   Model.initFirebase();
-  Model.signIn();
+  Model.signIn(initListeners);
 });
--- homework5/public/model/model.js
+++ homework5/public/model/model.js
@@ -12,11 +12,31 @@ export function initFirebase() {
   });
 }
 
-export function signIn() {
+export function signIn(callback) {
   firebase
     .auth()
     .signInAnonymously()
     .then(function(result) {
       _db = firebase.firestore();
+      callback();
+    });
+}
+
+export function getStudentByClassNumber(genre) {
+  $(".content").html("");
+  $(".content").append(`<h3>${genre}</h3>`);
+
+  _db
+    .collection("Albums")
+    .where("genre", "==", genre)
+    .get()
+    .then(function(querySnapshot) {
+      console.log(querySnapshot);
+      querySnapshot.forEach(function(doc) {
+        let album = doc.data();
+        $(".content").append(
+          `<p> Album: ${album.albumName},  Artist:  ${album.artistName} <img src="${album.coverArt}" </p>`
+        );
+      });
     });
 }
